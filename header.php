d61854637becd0658e5e41db6e2c8013512200f7
mariapobre <2221234@johnabbottcollege.net>
2025-04-08 10:36:30 -0400

    Update header.php

--- header.php
+++ header.php
@@ -16,6 +16,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?php // the viewport metatag is what makes a design responsive ?>
         
+    <!-- START 2.12 WP_HEAD GET DYNAMIC HEADER CONTENT -->
+
+    <?php wp_head(); ?>
+
+    <!-- END 2.12 WP_HEAD GET DYNAMIC HEADER CONTENT -->-
+
         <title></title>
     </head>
 
@@ -23,7 +29,26 @@
 
     <header>
         <nav>
+                <!-- START STEP 2.11 ADD THE MENU TO THE PAGE -->
+    <?php 
+        wp_nav_menu(array(
+            'menu_class'	  	=> 'primary-menu',  // adds: class="primary-menu"
+            'theme_location' 	=> 'header-menu',   // is controlled as the "header menu" in the backend
+            'menu_id'	    	=> 'menu',          // adds: id="menu"
+            'container'         => 'div',           // menu is inside a div
+        ));
+    ?>
+    <!-- END STEP 2.11 ADD THE MENU TO THE PAGE -->
         </nav>
     </header>
+    
+        <!-- START STEP 2.13 ADD SEARCH FORM -->
+
+        <?php get_search_form(); ?>
+
+<!-- END STEP 2.13 ADD SEARCH FORM -->       
+
     <!-- END STEP 2.4 ADD HEADER.PHP CODE -->
     <?php  // END OF STEP 2.4 ?>
+
+    
